6c090a7591f431c526ca93d71cb6fae8b5cace95
ngynv <becuadangyeu17@gmail.com>
2023-10-22 20:15:32 +0700

    22.10

---
+++ demoDOAN/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demoDOAN.Models;
+
+namespace demoDOAN.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        in4productEntities database = new in4productEntities();
+        // GET: Categories
+        public ActionResult Sanpham()
+        {
+            var categories = database.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ demoDOAN/Controllers/ThuonghieuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoDOAN.Controllers
+{
+    public class ThuonghieuController : Controller
+    {
+        // GET: Thuonghieu
+        public ActionResult Thuonghieu()
+        {
+            return View();
+        }
+    }
+}
